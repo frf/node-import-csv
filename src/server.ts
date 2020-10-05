7a98a0def5ac7ab00e7858daccc0963965d3cb35
Fabio <fabio@fabiofarias.com.br>
2020-10-05 17:19:07 -0300

    First

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,11 @@
 import express from 'express';
 import cors from 'cors';
 import routes from './routes/routes';
-import routesAuth from './routes/auth';
-import authMiddleware from './middlewares/auth';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('/api/auth', routesAuth);
-app.use('/api', authMiddleware, routes);
+app.use('/api', routes);
 
-app.listen(3333);
+app.listen(3000);
